3375e5de9968fa1bb635c48f77791fff2fd99232
andpli <pandre@tut.by>
2023-08-18 02:58:20 +0300

    fix: library3 init slider

--- library/js/index.js
+++ library/js/index.js
@@ -26,3 +26,56 @@ headerLink.forEach(link => link.addEventListener('click', function(e) {
     body.classList.remove('lock');
  }));
 
+let position = 0;
+let slidesToShow = 1;
+let slidesToScroll = 1;
+let containerWidth = 450
+
+let container = document.querySelector('.slider__container-tablet');
+let track = document.querySelector('.slider__track-tablet');
+let btnPrev = document.querySelector('.btnPrev');
+let btnNext = document.querySelector('.btnNext');
+let items = document.querySelectorAll('.slider__item-tablet');
+if (window.innerWidth >= 1440) {
+  slidesToShow = 3;
+  containerWidth = 1401;
+  container = document.querySelector('.slider__container');
+  track = document.querySelector('.slider__track');
+  items = document.querySelectorAll('.slider__item');
+  btnPrev = document.querySelector('.btnPrevM');
+  btnNext = document.querySelector('.btnNextM');
+};
+
+
+const itemsCount = items.length;
+const itemWidth = containerWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
+
+items.forEach((item) => {
+   item.style.minWidth = `${itemWidth}px`;
+});
+
+btnNext.addEventListener('click', () => {
+  const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+  position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  setPosition();
+  checkBtns();
+});
+
+btnPrev.addEventListener('click', () => {
+  const itemsLeft = Math.abs(position)  / itemWidth;
+  position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  setPosition();
+  checkBtns();
+});
+
+const setPosition = () => {
+   track.style.transform = `translateX(${position}px)`;
+};
+
+const checkBtns = () => {
+  btnPrev.disabled = position === 0;
+  btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
+
+checkBtns();
