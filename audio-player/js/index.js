dd9baa23261ef7a6f8b1a603f500870f3b801f23
andpli <pandre@tut.by>
2023-09-23 01:11:01 +0300

    feat: added playlist

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,7 +1,14 @@
-console.log("I'm here");
+console.log("Вёрстка +10");
+console.log("Кнопка Play/Pause +10");
+console.log("При кликах по кнопкам 'Вперёд' и 'Назад' переключается проигрываемый аудиотрек. Аудиотреки пролистываются по кругу - после последнего идёт первый +10");
+console.log("При смене аудиотрека меняется изображение - обложка аудиотрека +10");
+console.log("Прогресс-бар отображает прогресс проигрывания текущего аудиотрека. При перемещении ползунка вручную меняется текущее время проигрывания аудиотрека +10");
+console.log("Отображается продолжительность аудиотрека и его текущее время проигрывания +10");
+console.log("Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10");
 
 let iCur = 0;
 let isPause = true;
+let isList = false;
 let isOn = true;
 let prevVol;
 
@@ -18,6 +25,8 @@ let song = document.querySelector('.song');
 let tostart = document.querySelector('.tostart');
 let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
+let list = document.querySelector('.list');
+let items ;
 
 let arr = [['assets/mp3/inna-hot.mp3', '03:35', 'Inna', 'Hot', 'img1'], 
            ['assets/mp3/sting-the-windmills-of-your-mind.mp3', '04:18', 'Sting', 'The Windmills of Your Mind' , 'img2'],
@@ -25,14 +34,23 @@ let arr = [['assets/mp3/inna-hot.mp3', '03:35', 'Inna', 'Hot', 'img1'],
            ['assets/mp3/mika-relax-take-it-easy.mp3', '03:41', 'Mika', 'Relax, Take It Easy' , 'img3'],
            ['assets/mp3/minelli-rampampam.mp3', '03:20', 'Minelli', 'Rampampam' , 'img3'],
            ['assets/mp3/kylie-minogue-can-not-get-you-out-of-my-head.mp3', '03:47', 'Kylie Minogue', "Can't Get You Out Of My Head", 'img3'],
-           ['assets/mp3/luis-fonsi-feat-daddy-yankee-despacito.mp3', '03:48', 'Luis Fonsi feat. Daddy Yankee', 'Despacito' , 'img3']];
+           ['assets/mp3/luis-fonsi-feat-daddy-yankee-despacito.mp3', '03:48', 'Luis Fonsi feat. Daddy Yankee', 'Despacito' , 'img3'],
+           ['assets/mp3/britney-spears-3.mp3', '03:23', 'Britney Spears', '3', 'img3'],
+           ['assets/mp3/basic-element-touch-you-right-now.mp3', '03:14', 'Basic Element', 'Touch You Right Now', 'img3'],
+           ['assets/mp3/lady-gaga-bad-romance.mp3', '04:21', 'Lady Gaga', 'Bad Romance', 'img3'],
+           ['assets/mp3/the-offspring-want-you-bad.mp3', '03:22', 'The Offspring', 'Want You Bad', 'img3'],
+           ['assets/mp3/kesha-tik-tok.mp3', '03:21', 'Ke$ha', 'TiK ToK', 'img3'],
+           ['assets/mp3/shakira-whenever-wherever.mp3', '03:17', 'Shakira', 'Whenever, Wherever', 'img3']
+          ];
 
+    
 playButton.addEventListener("click", () => {play()});
 stopButton.addEventListener("click", () => {stop()});
 volButton.addEventListener("click", () => {vol()});
 volume.addEventListener("change", (el) => {setVolume(el.target.value);});
 duration.addEventListener("change", () => { setCurrent();});
 tostart.addEventListener("click", () => { audio.currentTime = 0;});
+list.addEventListener("click", () => {showlist()});
 audio.addEventListener('volumechange',() => {volume.value = audio.volume * 100;});
 audio.addEventListener('ended',() => {next.click();});
 audio.addEventListener('timeupdate', () => { timeUpdate(); });
@@ -41,28 +59,82 @@ window.onload = myinit();
 
 function myinit(){
   audio.src = arr[iCur][0];
+  audio.currentTime = 0;
   total.textContent = arr[iCur][1]; 
   artist.textContent = arr[iCur][2];
   song.textContent = arr[iCur][3];
+  addList();
+  items = document.querySelectorAll('.item')
+  items.forEach(link => link.addEventListener('click', (el) => {
+    if (iCur == Array.prototype.indexOf.call(items, el.target)){
+      return;
+    }
+    iCur = Array.prototype.indexOf.call(items, el.target);
+    audio.currentTime = 0;
+    audio.src = arr[iCur][0];  
+    playAudio();
+  }));  
+
+  selectCurrentItem();
 }
 
-next.addEventListener("click", () => {iCur ++; if (iCur >= arr.length) {iCur = 0};   audio.currentTime = 0;
- audio.src = arr[iCur][0];  
-  playAudio();
+function addList(){
+  let list = document.querySelector("ul");
+  let newNode;
+  let elemText;
+  let newSpan;
+  let spanText;
+  for (let li = 0; li < arr.length; li++){
+    newNode = document.createElement("li");
+    newNode.classList.add('item');
+    elemText = document.createTextNode(arr[li][2] + ' - ' + arr[li][3]);
+    newNode.appendChild(elemText);
+    newSpan = document.createElement('span');
+    newSpan.classList.add('list_time');
+    spanText = document.createTextNode(arr[li][1]);
+    newSpan.appendChild(spanText);
+    newNode.appendChild(newSpan);
+    list.appendChild(newNode);
+  }
+}
 
+function showlist(){
+  isList = !isList;
+  list.classList.toggle('liston');
+  document.querySelector('.player').style.width = (isList ? '800px' : '400px');
+  document.querySelector('.player_list').style.opacity  = (isList ? '0.6' : '0');
+}
+
+
+
+next.addEventListener("click", () => {iCur ++; if (iCur >= arr.length) {iCur = 0};   audio.currentTime = 0;
+  audio.src = arr[iCur][0];  
+  playAudio();
 });
 
 prev.addEventListener("click", () => {iCur --; if (iCur < 0) {iCur = arr.length - 1};   audio.currentTime = 0;
-audio.src = arr[iCur][0];    playAudio();
-
+  audio.src = arr[iCur][0];
+  playAudio();
 });
+
+function selectCurrentItem(){
+  let elements =  document.querySelectorAll('.item');
+  for (let li = 0; li < elements.length; li ++){
+      elements[li].style.opacity = (li == iCur ? 1 : 0.6);
+  }
+}
 
 function setCurrent(){
   audio.currentTime = audio.duration * duration.value / 100;
 }
 
 function timeUpdate(){
-  duration.value = audio.currentTime * 100 /audio.duration;
+  if (audio.duration > 0 ) {
+    duration.value = audio.currentTime * 100 /audio.duration; 
+  }
+  else {
+    duration.value = 0;
+  }
   current.textContent = convertTime(audio.currentTime);
 }
 
@@ -91,8 +163,7 @@ function setVolume(el){
     volButton.classList.add('off');
     isOn = false;
     volButton.title = 'Unmute';
-  }
-  else {
+  } else {
     volButton.classList.remove('off');
     volButton.classList.add('on');  
     isOn = true;
@@ -103,6 +174,7 @@ function setVolume(el){
 function playAudio() {
   document.querySelector('body').style.backgroundImage = `url('assets/alb/${iCur}/0.jpg')`;
   document.querySelector('.alb').srcset = `assets/alb/${iCur}/1.jpg`;
+  selectCurrentItem();
   if (isPause == false) {audio.currentTime = 0;}
   isPause = false;
   total.textContent = arr[iCur][1]; 
@@ -116,7 +188,6 @@ function playAudio() {
 }
 
 function pauseAudio() {
-
   audio.pause();
   isPause = true;
   playButton.classList.remove('pause');
